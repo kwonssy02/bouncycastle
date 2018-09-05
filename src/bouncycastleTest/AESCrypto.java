455445348f28864fb80fe9e5a3bdd4f44ef3f8c5
HYUK CHAN KWON <KHC@HYUKui-MacBook-Pro.local>
2018-09-05 22:23:17 +0900

    indentation 수정

--- src/bouncycastleTest/AESCrypto.java
+++ src/bouncycastleTest/AESCrypto.java
@@ -18,55 +18,55 @@ import org.bouncycastle.util.encoders.Base64;
 public class AESCrypto {
 
 	private static String algorithm = "AES/CBC/PKCS5Padding";
-//	private static String secretKey = "12345678901234567890123456789012";
+	//	private static String secretKey = "12345678901234567890123456789012";
 	private static String secretKey = "1234567890123456";
-    private static String iv = "1234567890123456";
+	private static String iv = "1234567890123456";
 
-    // base64는 아스키 코드 내로 표현가능한 인코딩 방식
-    private static final String ASCII = "US-ASCII";
-    
-    // 문자인코딩 방식
-    private final String charset = "UTF-8";
-    
+	// base64는 아스키 코드 내로 표현가능한 인코딩 방식
+	private static final String ASCII = "US-ASCII";
+
+	// 문자인코딩 방식
+	private final String charset = "UTF-8";
+
 	public String encrypt(String plainText) throws UnsupportedEncodingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
 		// 암호화 키 생성
-	    byte[] keyData = secretKey.getBytes(ASCII);
-	    SecretKey secureKey = new SecretKeySpec(keyData, "AES");
-	    
-	    // cipher instance 초기화
+		byte[] keyData = secretKey.getBytes(ASCII);
+		SecretKey secureKey = new SecretKeySpec(keyData, "AES");
+
+		// cipher instance 초기화
 		Cipher cipher = Cipher.getInstance(algorithm);
 		cipher.init(Cipher.ENCRYPT_MODE, secureKey, new IvParameterSpec(iv.getBytes(ASCII)));
-		
+
 		// Encode with UTF-8
 		byte[] encodedBytes = plainText.getBytes(charset);
-		
+
 		// Encrypt
 		byte[] encryptedBytes = cipher.doFinal(encodedBytes);
-		
+
 		// Encode with Base64
 		String encrypted = new String(Base64.encode(encryptedBytes));
-		
+
 		return encrypted;
 	}
-	
+
 	public String decrypt(String encrypted) throws UnsupportedEncodingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
 		// 암호화 키 생성 - keyEncoder를 이용
-	    byte[] keyData = secretKey.getBytes(ASCII);
-	    SecretKey secureKey = new SecretKeySpec(keyData, "AES");
-	    
-	    // cipher instance 초기화
+		byte[] keyData = secretKey.getBytes(ASCII);
+		SecretKey secureKey = new SecretKeySpec(keyData, "AES");
+
+		// cipher instance 초기화
 		Cipher cipher = Cipher.getInstance(algorithm);
 		cipher.init(Cipher.DECRYPT_MODE, secureKey, new IvParameterSpec(iv.getBytes(ASCII)));
-		
+
 		// Decode with Base64
 		byte[] decodedBytes = Base64.decode(encrypted);
-		
+
 		// Decrypt
 		byte[] plainBytes = cipher.doFinal(decodedBytes);
-		
+
 		// Decode with UTF-8
 		String decrypted = new String(plainBytes, charset);
-		
+
 		return decrypted;
 	}
 }
